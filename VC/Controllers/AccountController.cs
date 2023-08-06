435f0b09ac541049a21fe2fdea4b4385386701a0
51xel <20rud15@gmail.com>
2023-08-06 23:19:06 +0300

    Created SignIn #6

--- VC/Controllers/AccountController.cs
+++ VC/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using VC.Data;
+using VC.Models.DTOs.AccountDTOs;
+using VC.Services.IServices;
 
 namespace VC.Controllers
 {
@@ -7,5 +9,41 @@ namespace VC.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
+        private IAccountService _accountService { get; }
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> SignInAsync([FromBody] UserSignInRequestDTO userSignInRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var response = await _accountService.SignInAsync(userSignInRequest);
+
+                if (response == null)
+                {
+                    return Unauthorized("Invalid Email or Password");
+                }
+
+                return Ok(response);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
     }
+
 }
